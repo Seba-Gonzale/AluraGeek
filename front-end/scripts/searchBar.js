fbc07862f2dc602dbf6367f3426e20417dcba40c
Seba <gonzaleaugusto@gmail.com>
2023-05-31 21:36:51 -0300

    fix: search in product-details

--- front-end/scripts/searchBar.js
+++ front-end/scripts/searchBar.js
@@ -3,60 +3,61 @@ import client_service from "../api/client_service.js";
 const nav = document.querySelector(".nav");
 const searchBar = document.querySelector(".nav__busqueda");
 const inputSearchBar = document.querySelector(".nav__input-busqueda");
-const table = document.createElement("table");
 
-console.log(nav);
-table.style.position = "absolute";
-table.style.top = "100%";
-table.style.left = "50%";
-table.style.backgroundColor = "white";
-table.style.width = "50%";
-table.style.maxHeight = "50vh";
-table.style.overflow = "auto";
-table.style.transform = "translate(-50%, 0)";
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
+
+function blur() {
+  table.style.display = "none";
+}
+
+table.appendChild(tbody);
+table.classList.add("lookup-table");
+nav.insertAdjacentElement("beforeend", table);
 
 searchBar.addEventListener("click", () => {
   console.log("hola mundo");
 });
+
 inputSearchBar.addEventListener("click", (e) => {
   e.stopPropagation();
 });
+
 inputSearchBar.addEventListener("focus", (e) => {
   table.style.display = "block";
 });
+
 inputSearchBar.addEventListener("input", async (e) => {
   const response = await client_service.getServerData(
-    `/products?name_like=${e.target.value}`
+    `/products?name_like=${e.target.value}&_sort=name&_order=asc`
   );
-  // console.log(
-  //   (table.innerHTML = response
-  //     .map(
-  //       (res) =>
-  //         `<tr>
-  //           <td><img style="width: 40px" src="${res.image}" /></td>
-  //           <td>${res.name}</td>
-  //         </tr>`
-  //     )
-  //     .join(""))
-  // );
-  // console.log(table);
   if (response.length !== 0) {
-    // table.innerHTML = "";
-    table.innerHTML =
-      '<tbody style="border-spacing: 16px;">' +
-      response
-        .map(
-          (res) =>
-            `<tr>
-          <td><img style="width: 40px" src="${res.image}" /></td>
-          <td>${res.name}</td>
-        </tr>`
-        )
-        .join("") +
-      "</tbody>";
-    nav.insertAdjacentElement("beforeend", table);
+    tbody.innerHTML = "";
+    response
+      .map((res) => {
+        const tr = document.createElement("tr");
+        tr.classList.add("lookup-table__item");
+        tr.innerHTML = `<tr>
+            <td><img src="${res.image}" /></td>
+            <td>${res.name}</td>
+          </tr>`;
+        tr.addEventListener("click", (e) => {
+          location.href = `./pages/product-details.html?id=${res.id}`;
+        });
+        tbody.appendChild(tr);
+      })
+      .join("");
+  } else {
+    tbody.innerHTML = `<tr style="display: block; padding: 1rem; font-weight: 700; border-bottom: 3px solid red">
+            <td style="display:block; text-align: center">Producto no encontrado!</td>
+          </tr>`;
   }
 });
-inputSearchBar.addEventListener("blur", (e) => {
-  table.style.display = "none";
+
+table.addEventListener("mouseenter", (e) => {
+  inputSearchBar.removeEventListener("blur", blur);
+});
+table.addEventListener("mouseleave", (e) => {
+  inputSearchBar.focus();
+  inputSearchBar.addEventListener("blur", blur);
 });
